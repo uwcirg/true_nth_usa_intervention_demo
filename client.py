f1d1e7fa73afa869c0e4b63a0a3170044ee2bd5f
Ivan Cvitkovic <ivanc@uw.edu>
2015-06-10 04:10:43 -0700

    Add intervention API consumption

--- client.py
+++ client.py
@@ -1,7 +1,6 @@
 from flask import Flask, url_for, session, request, jsonify, redirect, render_template
 from flask_oauthlib.client import OAuth
 
-
 app = Flask(__name__, template_folder='templates')
 app.config.from_pyfile('application.cfg', silent=False)
 oauth = OAuth(app)
@@ -22,11 +21,17 @@ remote = oauth.remote_app(
 def index():
     # TODO: Need to validate remote_oauth - problems w/ expired
     if 'remote_oauth' in session:
-        resp = remote.get('me')
-        resp2 = remote.get('assessments')
-        #return jsonify(resp.data)
-	return render_template('client_home.html', PORTAL=app.config['PORTAL'],
-            data=resp.data, assessments=resp2.data)
+        me = remote.get('me')
+        assessments = remote.get('assessments')
+        clinical = remote.get('clinical')
+        
+	return render_template(
+        'client_home.html',
+        PORTAL=app.config['PORTAL'],
+        user=me.data,
+        assessments=assessments.data,
+        clinical=clinical.data,
+    )
 
     # Without 'remote_oauth' in session, we haven't yet authorized
     # this intervention as an OAuth client to the Portal.  Do so now:
